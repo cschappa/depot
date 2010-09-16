0104c994ad121104dd6dff80250104e0d0631b0e
Caleb Schappa <cschappa@yahoo.com>
2010-09-16 16:04:28 -0400

    cart in side bar

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'f188f3afa8fc308333b367533956a63d'
+  protect_from_forgery  :secret => 'f188f3afa8fc308333b367533956a63d'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
